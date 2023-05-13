49400e385d99288174121bfacb9d14f9e63ce60a
belaneb <belane.belayhun@gmail.com>
2023-05-13 18:08:14 -0500

    nav bar added, styling added

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const helpers = require ('./utils/helpers')
 const htmlRoutes = require("./controllers/html-routes")
 const path = require("path")
 const hbs = exphbs.create({helpers})
+const { Sequelize } = require('sequelize');
+//const { create } = require('./models/favorites')
 
 app.engine('handlebars', hbs.engine); //setting up hbs
 app.set('view engine', 'handlebars'); //setting up hbs
@@ -17,3 +19,27 @@ app.use("/", htmlRoutes)
 app.listen (PORT, () => {
     console.log("Server is Listening") //turning server on
 })
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('my_database', 'belane@localhost', 'newpassword', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+
+  
+//   async function createDbConnection(){
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//       } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//       }
+//   }
+//   createDbConnection();
+
+
+// sequelize.authenticate().then(()=>{
+// console.log('connection created successfully')
+// }).catch((error)=>{
+//     console.log(error)
+// })
