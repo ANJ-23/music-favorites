c8e8ca962c8dc9b78ab16d3f001c14d73acda9fe
morrispg <124636880+morrispg@users.noreply.github.com>
2023-05-13 15:54:51 -0500

    after tutor GET and POST working

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { Songs, User, Favorites } = require('../models');
+const { Songs, User } = require('../models');
 
 // import seed files
 const favoritesData = require('./favoritesData.json');
@@ -10,9 +10,10 @@ const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   // populates database tables via Sequelize
-  await Favorites.bulkCreate(favoritesData);
+  /* await Favorites.bulkCreate(favoritesData); */
   await Songs.bulkCreate(songsData);
   await User.bulkCreate(userData);
+  // force: true causes everything to drop
 
   process.exit(0);
 };
--- server.js
+++ server.js
@@ -1,19 +1,46 @@
-const express = require ('express')
-const app = express()
-const PORT = process.env.PORT || 3001
-const exphbs = require ('express-handlebars')
-const helpers = require ('./utils/helpers')
-const htmlRoutes = require("./controllers/html-routes")
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({helpers})
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.engine('handlebars', hbs.engine); //setting up hbs
-app.set('view engine', 'handlebars'); //setting up hbs
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.json()); //formatting/parcing requests coming in into json format
-app.use(express.urlencoded({ extended: false })); //changing the format of the url (replacing % w/ space)
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use("/", htmlRoutes)
-app.listen (PORT, () => {
-    console.log("Server is Listening") //turning server on
-})
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
